bedf42761c119c34416e1e33ef5e51aceb086724
Renan <warreir@gmail.com>
2024-04-21 19:33:35 -0300

    ajustando operacoes

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require('express')
-const { somar, sub } = require('./exercicios')
+const {
+    apiSomar,
+    apiSubtrair,
+    apiMultiplicar,
+    apiDividir
+} = require('./api/operacoes')
 
 const app = express()
 const porta = 3000
 app.use(express.json())
 
-app.post('/somar', (req, res) => {
-    const { num1, num2 } = req.body
-    const result = somar(num1, num2)
+app.post('/somar', apiSomar)
 
-    res.status(200).send({ result })
-})
+app.post('/sub', apiSubtrair)
 
-app.post('/sub', (req, res) => {
-    const { num1, num2 } = req.body
-    const result = sub(num1, num2)
+app.post('/mult', apiMultiplicar)
 
-    res.status(200).send({ result })
-})
-
+app.post('/div', apiDividir)
 
 app.listen(porta, () => {
     console.log('servidor rodando na porta ' + porta)
