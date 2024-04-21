bb69798987f2be42fd474b2f086f39fc988ad8c6
Renan <warreir@gmail.com>
2024-04-21 20:11:04 -0300

    testes

--- services/operacoes.js
+++ services/operacoes.js
@@ -1,13 +1,28 @@
 function somar(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)){
+        return "favor informar somente números"
+    }
     return num1 + num2
 }
 function sub(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)){
+        return "favor informar somente números"
+    }
     return num1 - num2
 }
 function mult(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)){
+        return "favor informar somente números"
+    }
     return num1 * num2
 }
 function div(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)){
+        return "favor informar somente números"
+    }
+    if(num2 == 0){
+        return 0
+    }
     return num1 / num2
 }
 
