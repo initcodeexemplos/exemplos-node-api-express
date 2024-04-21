12427566e3d62229bb2bc4d440a81afadc6b6182
Renan <warreir@gmail.com>
2024-04-21 18:45:02 -0300

    somar

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const { somar, sub } = require('./exercicios')
 
 const app = express()
 const porta = 3000
+app.use(express.json())
 
-app.get('/somar', (req, res) => {
-    const { num1, num2 } = req.query
-    res.send('A soma é: ' + (Number(num1) + Number(num2)))
+app.post('/somar', (req, res) => {
+    const { num1, num2 } = req.body
+    const result = somar(num1, num2)
+
+    res.status(200).send({ result })
 })
+
+app.post('/sub', (req, res) => {
+    const { num1, num2 } = req.body
+    const result = sub(num1, num2)
+
+    res.status(200).send({ result })
+})
+
 
 app.listen(porta, () => {
     console.log('servidor rodando na porta ' + porta)
