7657c1a52b48a8f9595f573e571497a6c97f3dc6
Renan <warreir@gmail.com>
2024-08-04 17:37:35 -0300

    api express throw

--- api/operacoes.js
+++ api/operacoes.js
@@ -6,31 +6,47 @@ const {
 } = require("../services/operacoes")
 
 function apiSomar(req, res) {
-    const { num1, num2 } = req.body
-    const result = somar(num1, num2)
-
-    res.status(200).send({ result })
+    try {
+        const { num1, num2 } = req.body
+        const result = somar(num1, num2)
+        
+        res.status(200).send({ result })
+    } catch (e) {
+        res.status(500).send({ msg: e.message })
+    }
 }
 
 function apiSubtrair(req, res) {
-    const { num1, num2 } = req.body
-    const result = sub(num1, num2)
+    try {
+        const { num1, num2 } = req.body
+        const result = sub(num1, num2)
 
-    res.status(200).send({ result })
+        res.status(200).send({ result })
+    } catch (e) {
+        res.status(500).send({ msg: e.message })
+    }
 }
 
 function apiMultiplicar(req, res) {
-    const { num1, num2 } = req.body
-    const result = mult(num1, num2)
-    
-    res.status(200).send({ result })
+    try {
+        const { num1, num2 } = req.body
+        const result = mult(num1, num2)
+        
+        res.status(200).send({ result })
+    } catch (e) {
+        res.status(500).send({ msg: e.message })
+    }
 }
 
 function apiDividir(req, res) {
-    const { num1, num2 } = req.body
-    const result = div(num1, num2)
-    
-    res.status(200).send({ result })
+    try {
+        const { num1, num2 } = req.body
+        const result = div(num1, num2)
+        
+        res.status(200).send({ result })
+    } catch (e) {
+        res.status(500).send({ msg: e.message })
+    }
 }
 
 module.exports = {
--- services/operacoes.js
+++ services/operacoes.js
@@ -1,13 +1,28 @@
 function somar(num1, num2) {
-    return num1 + num2
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar somente números");
+    }
+    return num1 - num2
 }
 function sub(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar somente números");
+    }
     return num1 - num2
 }
 function mult(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar somente números");
+    }
     return num1 * num2
 }
 function div(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar somente números");
+    }
+    if(num2 == 0) {
+        throw new Error("Não é possível dividir por zero");
+    }
     return num1 / num2
 }
 
